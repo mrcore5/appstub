e494b3cf27c12c57cc3f78232595ef1fb503e4ae
Matthew Reschke <mail@mreschke.com>
2015-08-05 10:55:57 -0500

    View rename and logo include

--- Views/welcome.blade.php
+++ Views/welcome.blade.php
@@ -1,29 +1,16 @@
-@extends('layout.app')
+@extends('appstub::layout')
+@include('layout._partials.interior-navigation')
 
 @section('css')
-	@parent
-	<style>
-		.container {
-			text-align: center;
-			vertical-align: middle;
-		}
-		.title {
-			font-size: 48px;
-			margin-bottom: 40px;
-		}
-		.quote {
-			font-size: 24px;
-			margin: 60px 0px 60px 0px;
-		}
-	</style>
+    @parent
 @stop
 
-@section('content')
-	@parent
-	<div class="container">
-		<div class="title">mRcore App</div>
+@section('wb-content')
+	@yield('interior-navigation')
+	<div class="quoteBox">
+		<div class="title">mRcore Application</div>
 		<a href="http://laravel.com" target="_blank">
-			<img src="http://laravel.com/assets/img/laravel-logo.png">
+			<img src="{{ asset('app/mrcore/appstub/images/laravel-logo.png') }}">
 		</a>
 		<div class="quote">{{ Inspiring::quote() }}</div>
 	</div>
@@ -31,7 +18,5 @@
 
 @section('script')
 	@parent
-	<script>
-		console.log('welcome!');
-	</script>
 @stop
+
