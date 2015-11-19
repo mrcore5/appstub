af5b3384cd343abdb21abec3dc040644d2583407
Matthew Reschke <mreschke@dynatronsoftware.com>
2015-11-19 11:44:08 -0600

    Commented AppCommand from service provider

--- Providers/AppstubServiceProvider.php
+++ Providers/AppstubServiceProvider.php
@@ -198,9 +198,9 @@ class AppstubServiceProvider extends ServiceProvider
 	protected function registerCommands()
 	{
 		if (!$this->app->runningInConsole()) return;
-		$this->commands([
-			\Mrcore\Appstub\Console\Commands\AppCommand::class
-		]);
+		#$this->commands([
+		#	\Mrcore\Appstub\Console\Commands\AppCommand::class
+		#]);
 	}
 
 	/**
